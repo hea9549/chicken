15108a2877d23c2dba9895845bde3e4f236bd206
yurimmm <ikisee@naver.com>
2017-09-16 03:16:30 +0900

    declare EditText and Button in MainActivity and connect Buttons to SearchListActivity

--- android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/MainActivity.java
+++ android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.nene.chicken.Presentation.Activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.nene.chicken.AppApplication;
 import com.nene.chicken.Model.TransPosition;
@@ -21,15 +27,53 @@ import java.util.List;
 public class MainActivity extends ChickenBaseActivity implements MainPresenter.View {
     MainPresenter presenter;
     private NMapView mMapView;// 지도 화면 View
+
+    private EditText fromEditText;
+    private EditText toEditText;
+    private Button fromButton;
+    private Button toButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setLayout();
         Log.e("좌표","" + NMapConverter.utmK2Grs(350111810, 149774298).getLatitude() + "," + NMapConverter.utmK2Grs(350111810, 149774298).getLongitude());
     }
 
     @Override
     public void drawPath(List<TransPosition> positions){
         // 화면그리기 작성해야함
+    }
+
+    private void setLayout(){
+        fromEditText = (EditText)findViewById(R.id.editText);
+        toEditText = (EditText)findViewById(R.id.editText2);
+
+        fromButton = (Button) findViewById(R.id.button);
+        fromButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard();
+                Intent intent = new Intent(MainActivity.this, SearchListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toButton = (Button) findViewById(R.id.button2);
+        toButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard();
+                Intent intent = new Intent(MainActivity.this, SearchListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
--- android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/SearchListActivity.java
+++ android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/SearchListActivity.java
@@ -1,11 +1,17 @@
 package com.nene.chicken.Presentation.Activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nene.chicken.R;
 
-public class SearchListActivity extends AppCompatActivity {
+public class SearchListActivity extends Activity {
+
+    ListView searchListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
