eb0fa0d0a45a875a5d59cde6f62a4a17ee52f55c
yurimmm <ikisee@naver.com>
2017-09-16 02:59:52 +0900

    add SearchListActivity and list_item_search before adding adapter

---
+++ android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/SearchListActivity.java
@@ -0,0 +1,15 @@
+package com.nene.chicken.Presentation.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nene.chicken.R;
+
+public class SearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_list);
+    }
+}
