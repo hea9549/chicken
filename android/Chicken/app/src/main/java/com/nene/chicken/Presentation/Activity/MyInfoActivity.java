2ba392f095b060be6abded26c081e7c83b84fa63
yurimmm <ikisee@naver.com>
2017-09-17 10:55:39 +0900

    amend myinfo.xml

--- android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/MyInfoActivity.java
+++ android/Chicken/app/src/main/java/com/nene/chicken/Presentation/Activity/MyInfoActivity.java
@@ -2,14 +2,26 @@ package com.nene.chicken.Presentation.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.nene.chicken.R;
 
 public class MyInfoActivity extends ChickenBaseActivity {
 
+    private RelativeLayout backLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_info);
+
+        backLayout = (RelativeLayout) findViewById(R.id.back_Layout);
+        backLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
